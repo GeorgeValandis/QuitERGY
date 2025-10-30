df7ae8f5fed3f002938bc232de784b940012ee5d
Georg A <georgios.avenidis@gmail.com>
2025-10-30 09:08:13 +0100

    Initial Commit

---
+++ QuitERGYTests/QuitERGYTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuitERGYTests.swift
+//  QuitERGYTests
+//
+//  Created by Georgios Avenidis on 30.10.25.
+//
+
+import Testing
+@testable import QuitERGY
+
+struct QuitERGYTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
